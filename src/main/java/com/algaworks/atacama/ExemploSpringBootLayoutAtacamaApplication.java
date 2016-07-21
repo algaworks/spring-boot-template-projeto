92038476a29b53dbc9f7aecd01db7da2704e07b8
Normandes Junior <normandesjr@gmail.com>
2016-07-21 18:03:57 -0300

    Primeiro commit

---
+++ src/main/java/com/algaworks/atacama/ExemploSpringBootLayoutAtacamaApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.atacama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringBootLayoutAtacamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploSpringBootLayoutAtacamaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/algaworks/atacama/controller/ProdutosController.java
@@ -0,0 +1,20 @@
+package com.algaworks.atacama.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutosController {
+
+	@RequestMapping("/novo")
+	public String novo() {
+		return "/produto/CadastroProduto";
+	}
+	
+	@RequestMapping
+	public String pesquisa() {
+		return "/produto/PesquisaProdutos";
+	}
+	
+}
